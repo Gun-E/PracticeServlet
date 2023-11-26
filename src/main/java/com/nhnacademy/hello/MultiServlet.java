219d30946f57fad7b189f236f135dbcd5e50b886
rkdrjsrks <rkdrjs71@naver.com>
2023-11-26 23:03:44 +0900

    feat: 파일 추가

--- src/main/java/com/nhnacademy/hello/MultiServlet.java
+++ src/main/java/com/nhnacademy/hello/MultiServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/multiServlet")
+@WebServlet(name = "multiServlet",urlPatterns = "/multi")
 public class MultiServlet extends HttpServlet {
     private static final Logger log = Logger.getLogger(MultiServlet.class.getName());
     @Override
