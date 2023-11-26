777a12d58ce9d415fc6ae345424004444cc06823
rkdrjsrks <rkdrjs71@naver.com>
2023-11-26 23:03:21 +0900

    feat: 파일 추가

--- src/main/java/com/nhnacademy/hello/login/LogoutServlet.java
+++ src/main/java/com/nhnacademy/hello/login/LogoutServlet.java
@@ -6,7 +6,7 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-@WebServlet("/logoutServlet")
+@WebServlet(name = "logoutServlet", urlPatterns = "/logout")
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
