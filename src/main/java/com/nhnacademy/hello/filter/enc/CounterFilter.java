7bd37cc3cdbfb82cb288d5e192e4e32f4182cb7a
rkdrjsrks <rkdrjs71@naver.com>
2023-11-27 09:58:01 +0900

    feat: 파일 추가

--- src/main/java/com/nhnacademy/hello/filter/enc/CounterFilter.java
+++ src/main/java/com/nhnacademy/hello/filter/enc/CounterFilter.java
@@ -3,7 +3,13 @@ package com.nhnacademy.hello.filter.enc;
 import com.nhnacademy.hello.CounterUtils;
 import java.io.IOException;
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
 
+@WebFilter(
+        filterName = "counterFilter",
+        urlPatterns = "/*"
+)
 public class CounterFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
